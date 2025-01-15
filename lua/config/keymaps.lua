c5f9ce870bd03530a614d09bfb0504e5b20c051e
kian <kianxavieragustin@gmail.com>
2025-01-15 14:59:34 +0800

    rosepine

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,8 @@
 --
 
 vim.keymap.set("n", "<leader>ch", ":Cheat<CR>", { noremap = true, silent = true, desc = "Cheat.sh" })
+vim.keymap.set("n", "<leader>zc", ":CMakeConfigure<CR>", { desc = "CMake Configure" })
+vim.keymap.set("n", "<leader>zb", ":CMakeBuild<CR>", { desc = "CMake Build" })
+vim.keymap.set("n", "<leader>zr", ":CMakeRun<CR>", { desc = "CMake Run" })
+vim.keymap.set("n", "<leader>zt", ":CMakeSelectTarget<CR>", { desc = "CMake Select Target" })
+vim.keymap.set("n", "<leader>zbt", ":CMakeSelectBuildType<CR>", { desc = "CMake Select Build Type" })
