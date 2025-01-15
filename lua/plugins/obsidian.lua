0945412868f4a6c6f9ebf9f325bb1a44f8e17aa9
kianxavier <kianxavieragustin@gmail.com>
2025-01-16 01:43:29 +0800

    added obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    -- obsidian
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Notes",
+          path = "~/Documents/Obsidian-Notes",
+        },
+      },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  },
+}
